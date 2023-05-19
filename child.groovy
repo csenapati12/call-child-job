94bc7fd36bfad67af9ffc4ab117a1f9510fcc072
Chaitanya Senapati <csenapati12@gmail.com>
2023-05-19 19:16:38 +0530

    Update child.groovy

--- child.groovy
+++ child.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('2nd job stage1') {
             steps {
-                echo "child 1 $DEPLOY_TO"
+                echo "child 11111111 ${params.DEPLOY_TO""
             }
         }
         stage('2nd job stage 2') {
