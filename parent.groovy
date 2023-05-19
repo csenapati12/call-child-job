b13c67274a449083be7d0d229f9a7463b06a2974
Chaitanya Senapati <csenapati12@gmail.com>
2023-05-19 20:28:36 +0530

    Update parent.groovy

--- parent.groovy
+++ parent.groovy
@@ -13,7 +13,7 @@ pipeline {
         } **/
         stage('Stage Name') {
            steps {
-                build(job: 'childjob', parameters: [string(name: 'DEPLOY_TO', value: "chaitanya"),name: 'one', value: "test") ])
+                build(job: 'childjob', parameters: [string(name: 'DEPLOY_TO', value: "chaitanya")])
            }
         }
     }
